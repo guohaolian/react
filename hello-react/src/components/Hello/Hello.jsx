9d03da5156977a258f03e68a3d89eb58a53cd79f
guohaolian <1040071899@qq.com>
2025-11-12 16:52:49 +0800

    03

---
+++ hello-react/src/components/Hello/Hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hello.css";
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Hello React!</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
