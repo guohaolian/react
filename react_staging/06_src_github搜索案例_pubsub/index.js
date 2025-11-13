1bb99d9eaec29f133d9de198af43a420e3af805c
guohaolian <1040071899@qq.com>
2025-11-13 22:36:48 +0800

    04

--- react_staging/06_src_github搜索案例_pubsub/index.js
+++ react_staging/06_src_github搜索案例_pubsub/index.js
@@ -1,8 +1,19 @@
-//引入react核心库
-import React from 'react'
-//引入ReactDOM
-import ReactDOM from 'react-dom'
-//引入App
-import App from './App'
+/* import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
+ */
+
+// index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container); // React 18 API
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- react_staging/08_src_路由的基本使用/App.jsx
+++ react_staging/08_src_路由的基本使用/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import {Route} from 'react-router-dom'
-import Home from './pages/Home' //Home是路由组件
-import About from './pages/About' //About是路由组件
-import Header from './components/Header' //Header是一般组件
-import MyNavLink from './components/MyNavLink'
+import {Link,Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
 
 export default class App extends Component {
 	render() {
@@ -11,7 +9,7 @@ export default class App extends Component {
 			<div>
 				<div className="row">
 					<div className="col-xs-offset-2 col-xs-8">
-						<Header/>
+						<div className="page-header"><h2>React Router Demo</h2></div>
 					</div>
 				</div>
 				<div className="row">
@@ -23,8 +21,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
 							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							<MyNavLink to="/about">About</MyNavLink>
-							<MyNavLink to="/home">Home</MyNavLink>
+							<Link className="list-group-item" to="/about">About</Link>
+							<Link className="list-group-item" to="/home">Home</Link>
 						</div>
 					</div>
 					<div className="col-xs-6">
